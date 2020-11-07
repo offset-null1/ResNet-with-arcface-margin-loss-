48a99446abedfae72a30ccf071af852df6fadd12
offset-null1 <nirvindak23@gmail.com>
2020-11-07 23:20:20 +0530

    added model

---
+++ model/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <torch/torch.h>
+#include "include/loss.h"
+#include "include/resnet.h"
+
+int main(){
+
+    std::cout<<std::boolalpha;
+    std::cout<<1;
+    return 0;
+}
